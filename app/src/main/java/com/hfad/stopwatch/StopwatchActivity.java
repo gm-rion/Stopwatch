d7d7903619baae262f7200abe70a8097c46cee55
Проданюк Владимир Анатольевич <v.prodaniuk@office.opticom.local>
2019-10-23 20:01:57 +0300

    first commit

---
+++ app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -0,0 +1,32 @@
+package com.hfad.stopwatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StopwatchActivity extends Activity {
+
+    private int seconds = 0;
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+
+    //Start the stopwatch running when the Start button is clicked.
+    public void onClickStart(View view) {
+        running = true;
+    }
+    //Stop the stopwatch running when the Stop button is clicked.
+    public void onClickStop(View view) {
+        running = false;
+    }
+    //Reset the stopwatch when the Reset button is clicked.
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+}
